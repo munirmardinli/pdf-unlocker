ce9458d8e04638a8e74615b1afa0b1232b200b57
Munir Mardinli <munir.mardinli@icloud.com>
2026-09-22 06:29:44 +0200

    add onrender

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,11 @@
 import type { NextConfig } from "next";
 
-const isGithubPages =
-  process.env.GITHUB_ACTIONS === 'true' ||
-  Boolean(process.env.NEXT_PUBLIC_BASE_PATH);
-
-const basePath = isGithubPages ? process.env.NEXT_PUBLIC_BASE_PATH || '/pdf-unlocker' : '';
-
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: false,
   trailingSlash: true,
-  basePath,
-  assetPrefix: basePath || undefined,
+  // basePath,
+  // assetPrefix: basePath || undefined,
   images: {
     unoptimized: true,
   },
